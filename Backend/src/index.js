db62f40ca70ec8f9ce5f9f2af0ec4a07d44e0468
Ayush Ghavghave <ghavghaveayush782@gmail.com>
2025-10-19 00:30:42 +0530

    fix: update default server port to 3000 in index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -8,8 +8,8 @@ import { app } from "./app.js";
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`\n Server is running at port : ${process.env.PORT || 8000}`);
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`\n Server is running at port : ${process.env.PORT || 3000}`);
     });
   })
   .catch((err) => {

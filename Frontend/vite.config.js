b8ac99e416021c5feaf529f15c79f4396a63b4a3
Ayush Ghavghave <ghavghaveayush782@gmail.com>
2025-10-18 23:55:31 +0530

    feat: add dotenv dependency and update API base URL configuration

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,17 +7,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  server: {
-    port: 5173,
-    proxy: {
-      // forward /api/* to backend
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  },
   resolve: {
     alias: {
       "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./src"),
